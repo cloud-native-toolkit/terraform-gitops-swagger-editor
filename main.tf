a6f4a81462a1b83269a9acf72d77071277d10e31
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-06-17 23:10:53 +0530

    Updated module name
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  name          = "my-module"
+  name          = "swaggereditor"
   bin_dir       = module.setup_clis.bin_dir
   yaml_dir      = "${path.cwd}/.tmp/${local.name}/chart/${local.name}"
   service_url   = "http://${local.name}.${var.namespace}"
