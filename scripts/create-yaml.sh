73f25c93de3ae8435168d8048bbd1b04863f218f
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-06-20 12:53:23 +0530

    Changes to fix gitops repo creation
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -5,7 +5,7 @@ MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
 CHART_DIR=$(cd "${SCRIPT_DIR}/../chart/swaggereditor"; pwd -P)
 
 NAME="$1"
-DEST_DIR="$2"
+DEST_DIR="$2" 
 
 ## Add logic here to put the yaml resource content in DEST_DIR
 mkdir -p "${DEST_DIR}"
@@ -15,3 +15,5 @@ if [[ -n "${VALUES_CONTENT}" ]]; then
   echo "${VALUES_CONTENT}" > "${DEST_DIR}/values.yaml"
 fi
 find "${DEST_DIR}" -name "*"
+echo "Files in output path"
+ls -l "${DEST_DIR}"
