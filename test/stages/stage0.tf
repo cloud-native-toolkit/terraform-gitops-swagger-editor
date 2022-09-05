88c28d015d7f1086833aaa0ac67126400e0057a7
Sean Sundberg <seansund@us.ibm.com>
2022-09-05 11:32:13 -0500

    Changes to use gitops provider (#3)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -1,7 +1,17 @@
 terraform {
+  required_version = ">= 0.15.0"
+
+  required_providers {
+    ibm = {
+      source = "ibm-cloud/ibm"
+    }
+    gitops = {
+      source = "cloud-native-toolkit/gitops"
+    }
+  }
 }
 
-module setup_clis {
+module setup_test_clis {
   source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
 
   bin_dir = "${path.cwd}/test_bin_dir"
@@ -11,5 +21,5 @@ module setup_clis {
 resource local_file bin_dir {
   filename = "${path.cwd}/.bin_dir"
 
-  content = module.setup_clis.bin_dir
+  content = module.setup_test_clis.bin_dir
 }
--- test/stages/stage1-capture-state.tf
+++
@@ -1,8 +0,0 @@
-module "dev_capture_state" {
-  source = "github.com/ibm-garage-cloud/terraform-k8s-capture-state"
-
-  cluster_type             = module.dev_cluster.platform.type_code
-  cluster_config_file_path = module.dev_cluster.config_file_path
-  namespace                = module.gitops_namespace.name
-  output_path              = "${path.cwd}/cluster-state/before"
-}
